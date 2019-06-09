5477daecae3ce8643d4ed7043c0944d996990a31
raelga <me@rael.io>
2019-06-09 11:22:58 +0000

    Add basepath to the start script

--- docker/eu.gcr.io/bigotes-pro/cms.bigot.es/start.sh
+++ docker/eu.gcr.io/bigotes-pro/cms.bigot.es/start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+BASE_PATH="$(dirname $(readlink -f $0))"
 
 docker run --detach --restart always \
   --name bigotes \
   --label 'traefik.frontend.rule=Host:cms.bigot.es' \
-  --env-file $(pwd)/env/bigotes.env \
-  --volume $(pwd)/context/wp-content/themes/:/var/www/html/wp-content/themes \
+  --env-file ${BASE_PATH}/env/bigotes.env \
+  --volume ${BASE_PATH}/context/wp-content/themes/:/var/www/html/wp-content/themes \
   eu.gcr.io/bigotes-pro/cms.bigot.es:dev
