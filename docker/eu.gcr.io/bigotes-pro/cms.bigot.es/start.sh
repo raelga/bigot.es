c9700f3e0a60e1637a3f211db59a0cbf84dfd8b1
raelga <me@rael.io>
2019-06-09 12:06:07 +0000

    Use envo-magazine from upstream

--- docker/eu.gcr.io/bigotes-pro/cms.bigot.es/start.sh
+++ docker/eu.gcr.io/bigotes-pro/cms.bigot.es/start.sh
@@ -5,5 +5,5 @@ docker run --detach --restart always \
   --name bigotes \
   --label 'traefik.frontend.rule=Host:cms.bigot.es' \
   --env-file ${BASE_PATH}/env/bigotes.env \
-  --volume ${BASE_PATH}/context/wp-content/themes/:/var/www/html/wp-content/themes \
+  --volume ${BASE_PATH}/context/wp-content/themes/envo-magazine-bigotes:/var/www/html/wp-content/themes/envo-magazine-bigotes \
   eu.gcr.io/bigotes-pro/cms.bigot.es:dev
