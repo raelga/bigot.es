f1ea6ac0c95417b7511637ed45b546957ba5ef36
raelga <me@rael.io>
2019-06-09 10:56:10 +0000

    Add bigot.es custom footer

--- docker/eu.gcr.io/bigotes-pro/cms.bigot.es/context/wp-content/themes/envo-magazine-bigotes/functions.php
+++ docker/eu.gcr.io/bigotes-pro/cms.bigot.es/context/wp-content/themes/envo-magazine-bigotes/functions.php
@@ -12,3 +12,43 @@ function envo_magazine_child_enqueue_styles() {
 	wp_enqueue_style( 'envo-magazine-child-style', get_stylesheet_uri(), array( 'envo-magazine-stylesheet' ) );
 
 }
+
+
+add_action( 'envo_magazine_generate_footer', 'envo_magazine_generate_construct_footer' );
+
+function envo_magazine_generate_construct_footer() {
+	?>
+	<div class="footer-credits-text text-center">
+		<?php 
+		printf( esc_html__( '%s' ), '<a href="https://google.com/">Condiciones de uso</a>' );
+		?>
+		<span class="sep"> | </span>
+		<?php 
+		printf( esc_html__( '%s' ), '<a href="https://google.com/">Política de cookies</a>' );
+		?>
+		<span class="sep"> | </span>
+		<?php 
+		printf( esc_html__( '%s' ), '<a href="https://google.com/">Redacción</a>' );
+		?>
+		<span class="sep"> | </span>
+		<?php 
+		printf( esc_html__( '%s' ), '<a href="https://google.com/">Contacto</a>' );
+		?>
+		<hr>
+		<?php 
+		printf( esc_html__( '%s %s' ), date("Y"), '<a href="https://google.com/">bigot.es</a>' );
+		?>
+	</div> 
+	<?php
+}
+
+register_sidebar(
+	array(
+		'name'			 => esc_html__( 'bigot.es Footer' ),
+		'id'			 => 'bigotes-footer',
+		'before_widget'	 => '<div id="%1$s" class="widget %2$s col-md-12">',
+		'after_widget'	 => '</div>',
+		'before_title'	 => '<div class="widget-title"><h3>',
+		'after_title'	 => '</h3></div>',
+	)
+);
