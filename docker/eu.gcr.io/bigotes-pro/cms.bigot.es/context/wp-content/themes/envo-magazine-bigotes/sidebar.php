365e71d6a0680e2a6713fd77bd8998430f6cef15
raelga <me@rael.io>
2019-06-15 21:34:03 +0000

    Customize sidebar with the author section

---
+++ docker/eu.gcr.io/bigotes-pro/cms.bigot.es/context/wp-content/themes/envo-magazine-bigotes/sidebar.php
@@ -0,0 +1,24 @@
+<?php if ( is_active_sidebar( 'envo-magazine-right-sidebar' ) ) { ?>
+	<aside id="sidebar" class="col-md-4">
+		<?php
+		$authordesc = get_the_author_meta( 'description' );
+		if ( !empty( $authordesc ) ) {
+			?>
+			<div class="widget postauthor-container">			  
+				<div class="widget-title">
+					<h3 class="about">
+						<?php esc_html_e( 'Sobre ', 'envo-magazine' ); ?>
+						<?php the_author_posts_link(); ?>
+					</h3>			
+				</div>        	
+				<div class="postauthor-content">	             						           
+					<p>
+						<?php the_author_meta( 'description' ) ?>
+					</p>					
+				</div>	 		
+			</div>
+
+		<?php } ?>
+		<?php dynamic_sidebar( 'envo-magazine-right-sidebar' ); ?>
+	</aside>
+<?php } ?>
