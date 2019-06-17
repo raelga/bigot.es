28554f294c6ff647263ae521d6d9ef1b1b575bbc
raelga <me@rael.io>
2019-06-17 01:14:15 +0000

    Add Google Analytics snippet

--- docker/eu.gcr.io/bigotes-pro/cms.bigot.es/context/wp-content/themes/envo-magazine-bigotes/header.php
+++ docker/eu.gcr.io/bigotes-pro/cms.bigot.es/context/wp-content/themes/envo-magazine-bigotes/header.php
@@ -5,6 +5,16 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129735875-1"></script>
+		<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-129735875-1');
+		</script>
+		<!-- Google AdSense -->
 		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 		<script>
 		(adsbygoogle = window.adsbygoogle || []).push({
