6ce5da66c3ebcc37b3b94e6b63d3e4d666e30b15
raelga <me@rael.io>
2019-06-26 21:51:24 +0000

    Remove author from from the content-single page

--- docker/eu.gcr.io/bigotes-pro/cms.bigot.es/context/wp-content/themes/bigotes/content-single.php
+++ docker/eu.gcr.io/bigotes-pro/cms.bigot.es/context/wp-content/themes/bigotes/content-single.php
@@ -1,45 +1,32 @@
 <!-- start content container -->
 <div class="row">      
-	<article class="col-md-<?php envo_magazine_main_content_width_columns(); ?>">
+	<article class="col-md-12">
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>                         
 				<div <?php post_class(); ?>>
 					<?php envo_magazine_thumb_img( 'envo-magazine-single', '', false, true ); ?>
 					<?php the_title( '<h1 class="single-title">', '</h1>' ); ?>
-					<?php envo_magazine_widget_date_comments(); ?>
-					<?php envo_magazine_author_meta(); ?>
-					<div class="single-content"> 
+					<?php bigotes_author_meta(); ?>
+
+					<div class="single-content col-md-<?php envo_magazine_main_content_width_columns(); ?>"> 
 						<div class="single-entry-summary">
 							<?php do_action( 'envo_magazine_before_content' ); ?> 
 							<?php the_content(); ?> 
 							<?php do_action( 'envo_magazine_after_content' ); ?> 
 						</div><!-- .single-entry-summary -->
-						<?php wp_link_pages(); ?>
-						<?php envo_magazine_entry_footer(); ?>
 					</div>
+					<?php get_sidebar( 'right' ); ?> 
+					<?php wp_link_pages(); ?>
+					<?php envo_magazine_entry_footer(); ?>
 					<?php envo_magazine_prev_next_links(); ?>
-					<?php
-					$authordesc = get_the_author_meta( 'description' );
-					if ( !empty( $authordesc ) ) {
-						?>
-						<div class="single-footer row">
-							<div class="col-md-4">
-								<?php get_template_part( 'template-parts/template-part', 'postauthor' ); ?>
-							</div>
-							<div class="col-md-8">
-								<?php comments_template(); ?> 
-							</div>
-						</div>
-					<?php } else { ?>
-						<div class="single-footer">
-							<?php comments_template(); ?> 
-						</div>
-					<?php } ?>
+					<div class="single-footer">
+						<?php comments_template(); ?> 
+					</div>
 				</div>        
-			<?php endwhile; ?>        
+			<?php endwhile; ?>
 		<?php else : ?>            
 			<?php get_template_part( 'content', 'none' ); ?>        
 		<?php endif; ?>    
 	</article> 
-	<?php get_sidebar( 'right' ); ?>
+	<!-- <?php get_sidebar( 'right' ); ?> -->
 </div>
 <!-- end content container -->
