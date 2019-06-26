237cd1deacd71ea56814a0b933bf61099380f75c
raelga <me@rael.io>
2019-06-26 21:47:03 +0000

    Add AdSense inline script

--- docker/eu.gcr.io/bigotes-pro/cms.bigot.es/context/wp-content/themes/bigotes/header.php
+++ docker/eu.gcr.io/bigotes-pro/cms.bigot.es/context/wp-content/themes/bigotes/header.php
@@ -14,6 +14,14 @@
 
 		gtag('config', 'UA-129735875-1');
 		</script>
+		<!-- Google AdSense -->
+		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<script>
+		(adsbygoogle = window.adsbygoogle || []).push({
+			google_ad_client: "ca-pub-6758041426106533",
+			enable_page_level_ads: true
+		});
+		</script>
 		<?php wp_head(); ?>
 	</head>
 	<body id="blog" <?php body_class(); ?>>
