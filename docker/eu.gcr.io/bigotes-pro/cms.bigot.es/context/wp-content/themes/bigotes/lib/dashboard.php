b3bd1607e757f55f95da1e62ae6344d04c1209a3
raelga <me@rael.io>
2019-06-26 21:37:27 +0000

    Remove PRO section from envo-magazine theme

--- docker/eu.gcr.io/bigotes-pro/cms.bigot.es/context/wp-content/themes/bigotes/lib/dashboard.php
+++ docker/eu.gcr.io/bigotes-pro/cms.bigot.es/context/wp-content/themes/bigotes/lib/dashboard.php
@@ -25,9 +25,9 @@ add_action( 'admin_menu', 'envo_magazine_theme_info' );
 
 function envo_magazine_theme_info() {
 
-	$menu_title = esc_html__( 'Envo Magazine theme', 'envo-magazine' );
+	$menu_title = esc_html__( 'Bigotes Theme', 'envo-magazine' );
 
-	add_theme_page( esc_html__( 'Envo Magazine dashboard', 'envo-magazine' ), $menu_title, 'edit_theme_options', 'et_magazine', 'envo_magazine_theme_info_page' );
+	add_theme_page( esc_html__( 'Bigotes Dashboard', 'envo-magazine' ), $menu_title, 'edit_theme_options', 'et_magazine', 'envo_magazine_theme_info_page' );
 }
 
 /**
@@ -110,7 +110,6 @@ function envo_magazine_theme_info_page() {
 		<div class="about-text"><?php echo esc_html( $theme_data->Description ); ?></div>
 		<h2 class="nav-tab-wrapper">
 			<a href="?page=et_magazine" class="nav-tab<?php echo is_null( $tab ) ? ' nav-tab-active' : null; ?>"><?php echo esc_html( $theme_data->Name ); ?></a>
-			<a href="?page=et_magazine&tab=pro_version" class="nav-tab<?php echo $tab == 'pro_version' ? ' nav-tab-active' : null; ?>"><?php esc_html_e( 'PRO version', 'envo-magazine' ) ?></a>
 			<?php do_action( 'envo_magazine_admin_more_tabs' ); ?>
 		</h2>
 
@@ -159,31 +158,6 @@ function envo_magazine_theme_info_page() {
 						<img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/screenshot.png" alt="Theme Screenshot" />
 					</div>
 				</div>
-			</div>
-		<?php } ?>
-
-		<?php if ( $tab == 'pro_version' ) { ?>
-			<div class="pro-version-tab info-tab-content">
-				<h3>
-					<?php esc_html_e( 'To get these awesome features, upgrade to Envo Magazine PRO!', 'envo-magazine' ); ?>
-				</h3>
-				<a href="<?php echo esc_url( 'https://envothemes.com/product/envo-magazine-pro/' ); ?>" target="_blank">
-					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/lib/img/em-banner.jpg">
-				</a>
-				<ul>
-					<li><?php esc_html_e( 'Unlimited colors', 'envo-magazine' ); ?></li>
-					<li><?php esc_html_e( '600+ Google fonts', 'envo-magazine' ); ?></li>
-					<li><?php esc_html_e( 'New homepage widgets', 'envo-magazine' ); ?></li>
-					<li><?php esc_html_e( 'Advertising spaces', 'envo-magazine' ); ?></li>
-					<li><?php esc_html_e( 'Mega Menu support', 'envo-magazine' ); ?></li>
-					<li><?php esc_html_e( 'Footer credits editor', 'envo-magazine' ); ?></li>
-					<li><?php esc_html_e( 'And much more...', 'envo-magazine' ); ?></li>
-				</ul>
-				<p>
-					<a href="<?php echo esc_url( 'https://envothemes.com/product/envo-magazine-pro/' ); ?>" target="_blank" class="button button-primary">
-						<?php esc_html_e( 'Envo Magazine PRO', 'envo-magazine' ); ?>
-					</a>
-				</p>
 			</div>
 		<?php } ?>
 
