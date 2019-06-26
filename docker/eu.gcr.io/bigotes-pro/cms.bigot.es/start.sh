bd9fa4145e86b88ec0d7a834c2a98d541addc174
raelga <me@rael.io>
2019-06-26 21:27:44 +0000

    Change bigotes theme folder

--- docker/eu.gcr.io/bigotes-pro/cms.bigot.es/start.sh
+++ docker/eu.gcr.io/bigotes-pro/cms.bigot.es/start.sh
@@ -8,4 +8,5 @@ docker run --detach --restart always \
   --label 'traefik.frontend.rule=Host:cms.bigot.es' \
   --env-file ${BASE_PATH}/env/bigotes.env \
   --volume ${BASE_PATH}/context/wp-content/themes/envo-magazine-bigotes:/var/www/html/wp-content/themes/envo-magazine-bigotes \
+  --volume ${BASE_PATH}/context/wp-content/themes/bigotes:/var/www/html/wp-content/themes/bigotes \
   eu.gcr.io/bigotes-pro/cms.bigot.es:dev
