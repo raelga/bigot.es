e43620a192090ee7bedc3cc2c9bc7ec45c83ec2e
Rael Garcia <rael@redhat.com>
2020-03-30 00:27:32 +0200

    Update adsense code

--- docker/eu.gcr.io/bigotes-pro/cms.bigot.es/context/wp-content/themes/bigotes/header.php
+++ docker/eu.gcr.io/bigotes-pro/cms.bigot.es/context/wp-content/themes/bigotes/header.php
@@ -6,9 +6,9 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+  <script data-ad-client="ca-pub-7522671868853833" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129735875-1"></script>
 	<script>window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); }; gtag('js', new Date()); gtag('config', 'UA-129735875-1');</script>
-	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 	<script>
 		(adsbygoogle = window.adsbygoogle || []).push({
 			google_ad_client: "ca-pub-7522671868853833",
